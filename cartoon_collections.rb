487fb021148ad4ec368852509f9ef5473dd14277
Wonjin <cwonjin1@gmail.com>
2020-12-12 10:29:58 -0500

    commit tree 0a80c22296178797b0e35114607909bdf3be69ed

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,40 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(dwarves)
+
+dwarves.each_with_index do |name, index|
+  puts "#{ index + 1 } #{name} \n"
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+
+
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.collect do |call|
+    call.capitalize + "!"
+
+  end
+
 end
 
-def find_the_cheese# code an argument here
+
+
+
+def long_planeteer_calls(array)# code an argument here
+  # Your code here
+  return true if array.any?{|word| word.length > 4}
+  false
+
+end
+
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
+
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
+
+
 end
